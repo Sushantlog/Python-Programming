3643f0a1f13b12ecbf61a6da06b0e70947a6c0c0
sushantbpatil53@gmail.com <sushantbptil53@gmail.com>
2022-12-10 22:06:32 +0530

    first commit

---
+++ String_11.py
@@ -0,0 +1,22 @@
+name="Anna"
+friend="Pavan"
+otherfrind="Parth"
+
+print("hello, "+name)       #print string
+
+#using tripal cot to use as string 
+apple='''he said 
+        hii suhant 
+        hey i am good           
+        "i want to eat an apple
+    '''
+print(apple)
+
+print(name[1])          #[] use location show
+print(name[0])
+
+#use large para 
+print("let use loop\n")
+
+for character in apple :
+    print(character)
